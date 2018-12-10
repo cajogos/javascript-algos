34d3596991e0338ba3c996ec4ae14182a2248c6d
Carlos Ferreira <carlosferreira@outlook.com>
2018-12-10 19:18:49 +0000

    Basics: Palindrome

---
+++ basic/palindrome.js
@@ -0,0 +1,32 @@
+function palindrome(str)
+{
+    let front = 0;
+    let back = str.length - 1;
+
+    while (back > front)
+    {
+        while (str[front].match(/[\W_]/))
+        {
+            front++;
+            continue;
+        }
+
+        while (str[back].match(/[\W_]/))
+        {
+            back--;
+            continue;
+        }
+
+        if (str[front].toLowerCase() !== str[back].toLowerCase())
+        {
+            return false;
+        }
+
+        front++;
+        back--;
+    }
+
+    return true;
+}
+
+console.log(palindrome('seyes'));
